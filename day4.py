1ed7809633faec2184094112e0ae0630fd57d1d9
wszy5 <wikoszyman@wp.pl>
2019-12-05 18:49:34 +0100

    part 2 of day4

--- day4.py
+++ day4.py
@@ -2,14 +2,17 @@
 T = [str(a) for a in range(147981,691424)]
 c = False #kryterium
 r = 0
+H = []
 for i in range(len(T)):
     for j in range(len(T[i])-1):
         if int(T[i][j])>int(T[i][j+1]):
             c = False
             break
-        if T[i][j]==T[i][j+1]:
+        if T[i][j]==T[i][j+1] and T[i].count(T[i][j])==2: #modify for 2 part
             c = True
     if c==True:
         r+=1
+        H.append(T[i])
         c = False
+
 print(r)
